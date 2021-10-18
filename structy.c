83771b6aabea8399e913928d1c7d2b083d36f97b
Viana Lin <vianalin@pop-os.localdomain>
2021-10-18 00:50:07 -0400

    done

--- structy.c
+++ structy.c
@@ -5,11 +5,35 @@
 struct order {
 	char * food;
 	double price;
+};
+
+void bill(struct order *o);
+struct order * ordering(char * food, double price);
+
+int main() {
+	srand(time(NULL));
+	char * food = "french fries";
+	double price = rand() / 1000000000.0;
+	
+	struct order *a = ordering(food, price);
+	bill(a);
+	
+	return 0;
 }
 
 void bill(struct order *o) {
 	int i;
-	printf("order: %c \n cost: %d \n", o->food, o->price);
+	printf("order: %s \n cost: $%d \n", o-> food, o->price);
 }
 
+struct order * ordering(char * food, double price) {
+	struct order *o;
+	o = malloc(sizeof(struct order));
 	
+	o->food = food;
+	o->price = price;
+	
+	return o;
+}
+
+
